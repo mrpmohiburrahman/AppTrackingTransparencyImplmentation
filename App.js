be184de49e5a48783951b4374a4c0e1281258469
mrpmohiburrahman <35300157+mrpmohiburrahman@users.noreply.github.com>
2021-05-20 11:01:03 +0600

    Access objc code using Native Modules

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import {NativeModules} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  async componentDidMount() {
+    let trackingStatus = await NativeModules.TrackingTransparency.getTrackingStatus();
+    console.log(`trackingStatus: ${trackingStatus}`)    
+    trackingStatus = await NativeModules.TrackingTransparency.requestTrackingPermission();
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text>
+          Requesting user-permission for App Tracking Transparency
+        </Text>
+      </SafeAreaView>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
